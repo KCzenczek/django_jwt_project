94ca75f5a6e3eefe2303732806690ebb18183771
Kinga Czenczek <kinga.czenczek@gmail.com>
2023-01-08 19:10:54 +0100

    new endpt user-list created

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+from api.views import UserList
+
 
 urlpatterns = [
+    path("v0/user-list", UserList.as_view(), name="user-list"),
     path("v0/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger_ui"),
     path("v0/schema/", SpectacularAPIView.as_view(), name="schema"),
 ]
