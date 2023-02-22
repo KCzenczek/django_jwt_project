823af547b8697abbdab24c431ec8e67c30ca0c83
Kinga Czenczek <kinga.czenczek@gmail.com>
2023-02-22 15:26:17 +0100

    #TODO api app

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from api.serializers import UserSerializer
 from rest_framework import generics
 
-
+#TODO finish endpoint auth
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
